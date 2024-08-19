2347704e0c41ba143e444da55dca34e7ebb21547
hotbrainy <nik50505@hotmail.com>
2024-08-19 15:53:58 +0300

    init -- project init

---
+++ pages/content-ui/tailwind.config.js
@@ -0,0 +1,7 @@
+const baseConfig = require('@extension/tailwindcss-config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...baseConfig,
+  content: ['src/**/*.{ts,tsx}'],
+};
